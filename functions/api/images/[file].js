af2573e27e106d9d169475e77666575239925585
Tom Hummel <tom@rayatheapp.com>
2022-10-30 22:37:05 -0700

    Get image file and cache in r2

--- functions/api/images/[file].js
+++ functions/api/images/[file].js
@@ -9,5 +9,19 @@ export async function onRequest(context) {
     data, // arbitrary space for passing data between middlewares
   } = context;
 
-  return new Response(`Hello, world! ${JSON.stringify(params, null, 2)}`);
+  let obj = await env.R2.get(params.file)
+  if (obj === null) {
+    const posterUrl = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${params.file}`
+    const res = await fetch(posterUrl)
+    env.R2.put(params.file, res.body)
+    obj = await env.R2.get(params.file)
+  }
+
+  const headers = new Headers()
+  obj.writeHttpMetadata(headers)
+  headers.set('etag', obj.httpEtag)
+
+  return new Response(obj.body, {
+    headers,
+  })
 }
